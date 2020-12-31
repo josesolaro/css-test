b719f7447432be2a4f65d212445f5939cbb43d82
josesolaro <josesolaro@gmail.com>
2020-12-31 11:04:20 -0300

    css test

---
+++ Reciever/Program.cs
@@ -0,0 +1,80 @@
+﻿using Amazon.SQS;
+using Amazon.SQS.Model;
+using System;
+using System.Threading.Tasks;
+
+namespace Reciever
+{
+    class Program
+    {
+
+        static string accessKey = "";
+        static string secretKey = "";
+        static string queueOwnerAWSAccountId = "";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press 'q' to exit and any other to continue reading");
+            var client = InitAWSSQSClient();
+            while (Console.ReadKey().KeyChar != 'q')
+            {
+                var response = RecieveMessageFromQueue(client);
+                if (response.Result == false)
+                {
+                    Console.WriteLine("Nothing to read for now");
+                }
+                Console.WriteLine("Press a key");
+            }
+        }
+
+
+        static private AmazonSQSClient InitAWSSQSClient()
+        {
+            var sqsConfig = new AmazonSQSConfig();
+
+            sqsConfig.ServiceURL = "http://sqs.us-east-2.amazonaws.com";
+
+            var credentials = new Amazon.Runtime.BasicAWSCredentials(accessKey, secretKey);
+
+            var client = new AmazonSQSClient(credentials, sqsConfig);
+
+            return client;
+        }
+
+        async static private Task<string> GetQueueUrl(AmazonSQSClient client)
+        {
+            var request = new GetQueueUrlRequest
+            {
+                QueueName = "SCS-test-project",
+                QueueOwnerAWSAccountId = queueOwnerAWSAccountId
+            };
+            var response = await client.GetQueueUrlAsync(request);
+            return response.QueueUrl;
+        }
+
+        async static private Task<bool> RecieveMessageFromQueue(AmazonSQSClient client)
+        {
+            try
+            {
+                var queueUrl = await GetQueueUrl(client);
+                var response = await client.ReceiveMessageAsync(queueUrl);
+                if (response.HttpStatusCode == System.Net.HttpStatusCode.OK && response.Messages.Count > 0)
+                {
+                    foreach (var message in response.Messages)
+                    {
+                        Console.WriteLine(message.Body);
+                        await client.DeleteMessageAsync(queueUrl, message.ReceiptHandle);
+                    }
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return false;
+        }
+    }
+
+}
---
+++ Sender/Program.cs
@@ -0,0 +1,75 @@
+﻿using Amazon.SQS;
+using Amazon.SQS.Model;
+using System;
+using System.Threading.Tasks;
+
+namespace Sender
+{
+    class Program
+    {
+        static string accessKey = "";
+        static string secretKey = "";
+        static string queueOwnerAWSAccountId = "";
+        static void Main(string[] args)
+        {
+            var awsClient = InitAWSSQSClient();
+
+            string message = "Insert the message you want to send, if you want to exit press enter\n";
+            while (message.Length != 0)
+            {
+                Console.WriteLine(message);
+                message = Console.ReadLine();
+                if (message.Length != 0)
+                {
+                    var httpResponse = SendMessageToQueue(awsClient, message);
+                    if (httpResponse.Result != System.Net.HttpStatusCode.OK)
+                    {
+                        Console.WriteLine("Fail To send Message. Exiting...");
+                        message = "";
+                    }
+                }
+            }
+        }
+
+        static private AmazonSQSClient InitAWSSQSClient()
+        {
+            var sqsConfig = new AmazonSQSConfig();
+
+            sqsConfig.ServiceURL = "http://sqs.us-east-2.amazonaws.com";
+
+            var credentials = new Amazon.Runtime.BasicAWSCredentials(accessKey, secretKey);
+
+            var client = new AmazonSQSClient(credentials, sqsConfig);
+
+            return client;
+        }
+
+        async static private Task<string> GetQueueUrl(AmazonSQSClient client)
+        {
+            var request = new GetQueueUrlRequest
+            {
+                QueueName = "SCS-test-project",
+                QueueOwnerAWSAccountId = queueOwnerAWSAccountId
+            };
+            var response = await client.GetQueueUrlAsync(request);
+            return response.QueueUrl;
+        }
+
+        async static private Task<System.Net.HttpStatusCode> SendMessageToQueue(AmazonSQSClient client, string message)
+        {
+            try
+            {
+                var queueUrl = await GetQueueUrl(client);
+
+                var messageRequest = new SendMessageRequest(queueUrl, message);
+                var response = await client.SendMessageAsync(messageRequest);
+                return response.HttpStatusCode;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return 0;
+        }
+    }
+}
